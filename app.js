ebf57084615ecb65441dca054395a9bd41be36a3
Rone47 <ryanbyrone47@gmail.com>
2023-10-07 20:42:09 +0300

    added code

--- app.js
+++ app.js
@@ -14,5 +14,50 @@ client.connect(function(err) {
 
     const db = client.db(dbName);
 
-    client.close();
+    findDocuments(db, function(){
+        client.close();
+    });
+
+    
 });
+
+const insertDocuments = function(db, callback) {
+
+    const collection = db.collection('fruits');
+
+    collection.insertMany([
+        {
+            name: "Apple",
+            score: 8,
+            review: "Great fruit"
+        },
+         {
+            name: "Orange",
+            score: 6,
+            review: "Kinda sour"
+         },
+          {
+            name: "Banana",
+            score: 9,
+            review: "Great stuff!"
+          }
+    ], function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("inserted 3 documents into the collection");
+        callback(result);
+});
+};
+
+const findDocuments = function(db, callback) {
+
+    const collection = db.collection('fruits');
+
+    collection.find({}).toArray(function(err, fruits){
+        assert.equal(err, null);
+        console.log("Found the following records");
+        consolelog(fruits);
+        callback(fruits);
+    });
+}
